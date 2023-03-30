a541857dc2bbe62cb855bbfb5c470a8e6aaf71d1
Jefferson Gabriel <jeffgabriiel@gmail.com>
2023-03-30 07:41:09 -0300

    continuando

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const path = require('path');
 const fs = require('fs');
+const port = 5000;
 
-app.listen(5000,() => {
-    console.log("servindo");
+app.listen(port,() => {
+    console.log("servindo na porta " + port);
 });
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.use('/public', express.static(path.join(__dirname, 'public'))); //conectar com a pasta ublic
+app.use('/public', express.static(path.join(__dirname, 'public'))); //conectar com a pasta public
 
 app.get('/', (req, res) => {
     res.render(path.join(__dirname+'/views/index.html'));
